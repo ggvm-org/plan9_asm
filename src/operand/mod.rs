41c0f98956622dfb1c228ae19666408e63971565
Krouton <me+git@tokinia.me>
2022-02-26 22:57:49 +0900

    Success $ cargo check

---
+++ src/operand/mod.rs
@@ -0,0 +1,82 @@
+use std::fmt;
+
+use crate::register_with_offset::Register;
+
+use super::register_with_offset::RegisterWithOffset;
+#[derive(Debug, PartialEq)]
+pub enum Operand {
+    Ident(String),
+    Int(i64),
+    RegisterWithOffset(RegisterWithOffset),
+}
+
+impl fmt::Display for Operand {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            Operand::Ident(s) => s.clone(),
+            Operand::Int(n) => format!("${n}"),
+            Operand::RegisterWithOffset(inner) => inner.to_string(),
+        };
+        write!(f, "{s}")
+    }
+}
+
+macro_rules! impl_from_operand {
+    ($from_ty:ty => Ident) => {
+        impl From<$from_ty> for Operand {
+            fn from(v: $from_ty) -> Self {
+                Self::Ident(v.to_string())
+            }
+        }
+    };
+    ($from_ty:ty => RegisterWithOffset) => {
+        impl From<$from_ty> for Operand {
+            fn from(v: $from_ty) -> Self {
+                Self::RegisterWithOffset(crate::register_with_offset::RegisterWithOffset::from(v))
+            }
+        }
+    };
+    ($from_ty:ty => $variant:ident) => {
+        impl From<$from_ty> for Operand {
+            fn from(v: $from_ty) -> Self {
+                Self::$variant(v)
+            }
+        }
+    };
+}
+
+impl_from_operand!(i64 => Int);
+impl_from_operand!(&str => Ident);
+impl_from_operand!(String => Ident);
+impl_from_operand!(RegisterWithOffset => RegisterWithOffset);
+impl_from_operand!(Register => RegisterWithOffset);
+
+#[macro_export]
+macro_rules! operand {
+    ($offset:expr => $register_variant:ident) => {
+        $crate::operand::Operand::RegisterWithOffset(crate::register_with_offset!($offset => $register_variant))
+    };
+    ($register:ident) => {
+       $crate::operand::Operand::RegisterWithOffset(crate::register_with_offset!($register))
+    };
+    ($expr:expr) => {
+        $crate::operand::Operand::from($expr)
+    };
+}
+
+#[cfg(test)]
+mod snapshots {
+    use super::*;
+    use insta::assert_display_snapshot;
+
+    macro_rules! insta_test {
+        ($testname:ident: $($testcases:expr),+) => {
+            #[test]
+            fn $testname() {
+                $(assert_display_snapshot!($testcases);)+
+            }
+        };
+    }
+
+    insta_test!(operand: operand!(AX), operand!(16=>SP), operand!(1));
+}
---
+++ src/register_with_offset/mod.rs
@@ -0,0 +1,77 @@
+use std::fmt;
+
+pub mod register;
+
+pub use register::Register;
+
+#[derive(Debug, PartialEq)]
+pub struct RegisterWithOffset {
+    pub offset: usize,
+    pub register: Register,
+}
+
+impl fmt::Display for RegisterWithOffset {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = {
+            let offset = self.offset;
+            let register = self.register;
+            if offset == 0 {
+                register.to_string()
+            } else {
+                format!("{offset}({register})")
+            }
+        };
+        write!(f, "{s}")
+    }
+}
+
+impl From<Register> for RegisterWithOffset {
+    fn from(register: Register) -> Self {
+        RegisterWithOffset {
+            offset: 0,
+            register,
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! register_with_offset {
+    // TODO: `=> $register_variant` to `($register_variant)`
+    ($offset:expr => $register_variant:ident) => {
+        $crate::register_with_offset::RegisterWithOffset {
+            offset: $offset,
+            register: $crate::register_with_offset::Register::$register_variant,
+        }
+    };
+    ($variant:ident) => {
+        $crate::register_with_offset::RegisterWithOffset {
+            offset: 0,
+            register: $crate::register_with_offset::Register::$variant,
+        }
+    }; // (1()2) => {};
+}
+
+#[cfg(test)]
+mod snapshots {
+    use super::*;
+    use insta::assert_display_snapshot;
+
+    macro_rules! insta_test {
+        ($testname:ident: $($testcases:expr),+) => {
+            #[test]
+            fn $testname() {
+                $(assert_display_snapshot!($testcases);)+
+            }
+        };
+    }
+
+    const TEST_OFFSET: usize = 10;
+
+    insta_test!(
+        register_with_offset: register_with_offset!(AX),
+        register_with_offset!(16=>SP),
+        register_with_offset!(TEST_OFFSET=>SP)
+    );
+}
+
+// register_with_offset!(1()2);
